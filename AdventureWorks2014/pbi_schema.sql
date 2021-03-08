c208a052f9576d084c56459d63f1d8ab19b37217
Brian Mather <BVMather@users.noreply.github.com>
2021-03-08 17:17:32 +0000

    Create pbi_schema.sql
    Create a schema for the Power BI views

---
+++ AdventureWorks2014/pbi_schema.sql
@@ -0,0 +1,5 @@
+/****** Object:  Schema [pbi]    Script Date: 08/03/2021 17:16:06 ******/
+
+/** Create a new schema pbi for the views we want to create for our Power BI model */
+CREATE SCHEMA [pbi]
+GO
